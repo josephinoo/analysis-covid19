a136e516925f35336a1374b9bbcd94ef5544e621
Joseph Avila Alvarez <josdavil@espol.edu.ec>
2020-12-06 23:54:21 -0500

    add:data

--- keywords.py
+++ keywords.py
@@ -1,6 +1,10 @@
-import os
-keywords = ['coronavirus', 'pandemia']
-countries = ['Caracas,Venezuela', 'Lima,Peru', 'Colombia', ""]
-for keyword in keywords:
-    os.system(
-        'twint --near Lima,Peru --search '+keyword+'  -o Lima_Peru_'+keyword+'.csv --csv --since 2020-01-01')
+import os 
+keywords = ['coronavirus', 'pandemia', "covid 19", "cuarentena", "casos"]
+countries = ['Honduras,Tegucigalpa',
+             'Nicaragua,Managua', 'Panama,Panama', 'Paraguay,Asuncion', 'Uruguay,Montevideo']
+for country in countries:
+    date = country.split(",")[0]
+    for keyword in keywords:
+
+        os.system(
+            'twint --near '+country + " " + "--search " + keyword + "  -o " + date + "_" + keyword+'.csv --csv --since 2020-01-01')
